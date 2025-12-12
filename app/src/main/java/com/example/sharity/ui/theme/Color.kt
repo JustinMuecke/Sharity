9d46bc15e49666c5700e67832340e3f5cacde662
Nick <of5150@kit.edu>
2025-12-12 21:46:27 +0100

    feat(design): adding colors and theme idea

--- app/src/main/java/com/example/sharity/ui/theme/Color.kt
+++ app/src/main/java/com/example/sharity/ui/theme/Color.kt
@@ -2,10 +2,28 @@ package com.example.sharity.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+// various LILAs
+val DarkBlackberry = Color(0xFF503459)
+val DustyPurple    = Color(0xFF81638B)
+val SheerLilac     = Color(0xFFB695C0)
+val GrapeGlimmer   = Color(0xFFDAC9DF)
+val PureWhite      = Color(0xFFFFFFFF)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+
+val AccentDeepIndigo = Color(0xFF5B557A)
+val AccentSoftGreyLilac = Color(0xFF767480)
+val AccentMutedPlum = Color(0xFF5A515D)
+
+val BackgroundDark = Color(0xFF1E1E1E)
+val SurfaceDark    = Color(0xFF26242B)
+val SurfaceVariantDark = Color(0xFF312E38)
+
+// Textcolors
+val OnPrimary        = Color(0xFF1E1E1E)
+val OnBackground     = PureWhite
+val OnSurface        = GrapeGlimmer
+val OnSurfaceVariant = Color(0xFFC2B3CF)
+
+// Status / Feedback
+val ErrorRed    = Color(0xFFCF6679)
+val SuccessGreen = Color(0xFF81C784)
