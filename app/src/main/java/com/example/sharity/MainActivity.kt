1f4bf52ce261fefdaeea76253946d4aded434627
Justin Mücke <justin.muecke@gmail.com>
2025-12-13 01:07:30 +0100

    feat(ui): Added Homescreen components

--- app/src/main/java/com/example/sharity/MainActivity.kt
+++ app/src/main/java/com/example/sharity/MainActivity.kt
@@ -6,22 +6,23 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.example.sharity.ui.theme.SharityTheme
+import com.example.sharity.ui.feature.homescreen.HomeScreen
+import com.example.sharity.ui.feature.homescreen.HomeScreenViewModel
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
+            val homeViewModel = viewModel<HomeScreenViewModel>()
             SharityTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    HomeScreen(
+                        viewModel = homeViewModel,
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -30,18 +31,5 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    SharityTheme {
-        Greeting("Android")
-    }
-}
+
