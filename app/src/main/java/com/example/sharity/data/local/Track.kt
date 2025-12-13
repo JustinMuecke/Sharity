1f4fe37458bc7ad2142fa0b25e3c3c4898d78a1a
1e99 <1e99@noreply.local>
2025-12-13 03:31:23 +0100

    fix(data): Make title no longer optional

--- app/src/main/java/com/example/sharity/data/local/Track.kt
+++ app/src/main/java/com/example/sharity/data/local/Track.kt
@@ -7,7 +7,7 @@ import androidx.room.PrimaryKey
 @Entity
 data class Track(
     @PrimaryKey @ColumnInfo(name = "content_uri") val contentUri: String,
-    @ColumnInfo(name = "title") val title: String?,
+    @ColumnInfo(name = "title") val title: String,
     @ColumnInfo(name = "artist") val artist: String?,
     @ColumnInfo(name = "release_year") val releaseYear: Int?,
     @ColumnInfo(name = "duration") val duration: Long?
