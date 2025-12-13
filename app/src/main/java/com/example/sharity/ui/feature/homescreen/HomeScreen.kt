5e92a114974ef7fd5ad91a4c392e70eeff18ba2e
Justin Mücke <justin.muecke@gmail.com>
2025-12-13 01:04:38 +0100

    feat(ui): Added HomeScreent incl. Tracklist

---
+++ app/src/main/java/com/example/sharity/ui/feature/homescreen/HomeScreen.kt
@@ -0,0 +1,39 @@
+package com.example.sharity.ui.feature.homescreen
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.example.sharity.ui.component.AudioControl
+import com.example.sharity.ui.component.SearchBar
+import com.example.sharity.ui.component.TagSelection
+import com.example.sharity.ui.component.TrackCard
+import com.example.sharity.ui.component.TrackList
+
+
+@Composable
+fun HomeScreen(viewModel : HomeScreenViewModel, modifier : Modifier){
+    Surface(
+        modifier = modifier.fillMaxSize(),
+        color = MaterialTheme.colorScheme.background
+    ) {
+        Column(
+            modifier = modifier.fillMaxSize() // Fill the screen
+        ) {
+            TagSelection()
+            SearchBar(viewModel)
+            TrackList(viewModel,
+                modifier = Modifier.weight(1f))
+            AudioControl(viewModel)
+        }
+    }
+}
